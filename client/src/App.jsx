efc432fb16c931ddb2a4e1f3273ca799b23a458c
Sankalp Kamble <sankalp.kamble@bridgenext.com>
2025-07-08 14:07:15 +0530

    Added Dashboard table, comment table . 3:02

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,12 +15,12 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog/:id" element={<Blog />} />
-        
-        <Route path='/admin' element={true ? <Layout/> : <Login/>}>
-          <Route index element={<Dashboard/>}/>
-          <Route path="addBlog" element={<AddBlog/>}/>
-          <Route path="listBlog" element={<ListBlog/>}/>
-          <Route path="comments" element={<Comments/>}/>
+
+        <Route path="/admin" element={true ? <Layout /> : <Login />}>
+          <Route index element={<Dashboard />} />
+          <Route path="addBlog" element={<AddBlog />} />
+          <Route path="listBlog" element={<ListBlog />} />
+          <Route path="comments" element={<Comments />} />
         </Route>
       </Routes>
     </div>
